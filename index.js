a423e5b85ca03d958e8a17f2044c67e7e1b889b2
esperancekariuki <kariukiesperance@gmail.com>
2022-12-11 08:25:05 +0300

    RE-SUBMITING ASSIGNMENT

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(toDo="rollerskate"){
+    return `This Saturday, I want to ${action}!`
+  }
+  let mondayWork = function(toDo="go to the office") {
+    return `This Monday, I will ${action}!`
+  }
+  let wrapAdjective = function(symbol="*") {
+    return function(adjective="special") {
+      return `You are ${symbol}${adjective}${symbol}!`
+    }
+}   
